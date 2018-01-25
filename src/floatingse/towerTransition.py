bb17a98febff4af00fbd519ab68939b94d879cb5
gbarter <garrett.barter@nrel.gov>
2018-01-25 16:42:02 -0700

    add new component for interface with tower

---
+++ src/floatingse/towerTransition.py
@@ -0,0 +1,32 @@
+from openmdao.api import Component
+import numpy as np
+
+class TowerTransition(Component):
+    """
+    OpenMDAO Component class for coupling between substructure and turbine tower
+    """
+
+    def __init__(self, nNodes, diamFlag=True):
+        super(TowerTransition,self).__init__()
+
+        self.diamFlag = diamFlag
+        
+        # Design variables
+        self.add_param('tower_metric', val=np.zeros((nNodes,)), units='m', desc='water depth')
+        self.add_param('base_metric', val=np.zeros((nNodes,)), units='m', desc='outer radius of tower at base')
+
+        # Output constraints
+        self.add_output('transition_buffer', val=0.0, units='m', desc='Buffer between substructure base and tower base')
+        
+    def solve_nonlinear(self, params, unknowns, resids):
+        r_tower = params['tower_metric']
+        r_base  = params['base_metric']
+
+        if self.diamFlag:
+            # Inputs were diameters and not radii, so we need to convert to radii
+            r_tower *= 0.5
+            r_base *= 0.5
+
+        # Constrain spar top to be at least greater than tower base
+        unknowns['transition_buffer'] = r_base[-1] - r_tower[0]
+    
