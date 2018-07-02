3674c31f66fdf2a8e59891de1de0ec1f02a1625d
gbarter <garrett.barter@nrel.gov>
2018-07-02 14:59:55 -0600

    adding tlp capabilities

--- src/floatingse/instance/spar_instance.py
+++ src/floatingse/instance/spar_instance.py
@@ -1,9 +1,7 @@
-from floating_instance import FloatingInstance, NSECTIONS, NPTS, vecOption
-from floatingse.floating import FloatingSE
-from commonse import eps
+from floating_instance import FloatingInstance
 import numpy as np
-import time
-        
+
+
 class SparInstance(FloatingInstance):
     def __init__(self):
         super(SparInstance, self).__init__()
@@ -59,72 +57,8 @@ class SparInstance(FloatingInstance):
         for k in poplist: conlist.pop(k)
 
         return conlist
-    '''
-        conlist = [
-            # Try to get tower height to match desired hub height
-            ['tow.height_constraint', None, None, 0.0],
-            
-            # Ensure that draft is greater than 0 (spar length>0) and that less than water depth
-            # Ensure that fairlead attaches to draft
-            ['base.draft_depth_ratio', 0.0, 0.75, None],
-            
-            # Ensure that the radius doesn't change dramatically over a section
-            ['base.manufacturability', None, 0.0, None],
-            ['base.weldability', None, 0.0, None],
-            ['tow.manufacturability', None, 0.0, None],
-            ['tow.weldability', None, 0.0, None],
-            
-            # Ensure that the spar top matches the tower base
-            ['sg.transition_buffer', -1.0, 1.0, None],
-            
-            # Ensure max mooring line tension is less than X% of MBL: 60% for intact mooring, 80% for damanged
-            ['mm.axial_unity', 0.0, 1.0, None],
-            
-            # Ensure there is sufficient mooring line length, MAP doesn't give an error about this
-            ['mm.mooring_length_max', None, 1.0, None],
-            
-            # API Bulletin 2U constraints
-            ['base.flange_spacing_ratio', None, 1.0, None],
-            ['base.stiffener_radius_ratio', None, 0.5, None],
-            ['base.flange_compactness', 1.0, None, None],
-            ['base.web_compactness', 1.0, None, None],
-            ['base.axial_local_api', None, 1.0, None],
-            ['base.axial_general_api', None, 1.0, None],
-            ['base.external_local_api', None, 1.0, None],
-            ['base.external_general_api', None, 1.0, None],
-            
-            # Pontoon stress safety factor
-            ['load.tower_stress', None, 1.0, None],
-            ['load.tower_shell_buckling', None, 1.0, None],
-            ['load.tower_global_buckling', None, 1.0, None],
-            
-            # Achieving non-zero variable ballast height means the semi can be balanced with margin as conditions change
-            ['subs.variable_ballast_height_ratio', 0.0, 1.0, None],
-            ['subs.variable_ballast_mass', 0.0, None, None],
-            
-            # Metacentric height should be positive for static stability
-            ['subs.metacentric_height', 0.1, None, None],
-            
-            # Center of buoyancy should be above CG (difference should be positive, None],
-            #['subs.buoyancy_to_gravity', 0.1, None, None],
-            
-            # Surge restoring force should be greater than wave-wind forces (ratio < 1, None],
-            ['subs.offset_force_ratio', None, 1.0, None],
-            
-            # Heel angle should be less than 6deg for ordinary operation, less than 10 for extreme conditions
-            ['subs.heel_moment_ratio', None, 1.0, None],
 
-            # Wave forcing period should be different than natural periods and structural modes
-            ['subs.period_margin_low', None, 1.0, None],
-            ['subs.period_margin_high', 1.0, None, None],
-            ['subs.modal_margin_low', None, 1.0, None],
-            ['subs.modal_margin_high', 1.0, None, None]
-        ]
-        return conlist
-    '''
-
-
-        
+    
     def visualize(self, fname=None):
         fig = self.init_figure()
 
